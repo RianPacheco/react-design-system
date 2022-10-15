1c4378773aec20fce4bd0f80ce4c85c1f6760e95
Rian_Pacheco <95894672+RianPacheco@users.noreply.github.com>
2022-10-15 14:28:52 -0300

    ci: Add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configtype}) =>{
+    if (configtype === 'PRODUCTION'){
+      config.base = '/react-design-system/'
+    }
+
+    return config
   }
 }
